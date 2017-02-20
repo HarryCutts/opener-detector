c72aae9e8549a4cd36ba88ac56c8a29e5150627f
Harry Cutts <eternal.linux@gmail.com>
2017-02-20 11:37:53 -0800

    Show an alert in the compromised tab

--- check.js
+++ check.js
@@ -1,6 +1,7 @@
 (function() {
 if (window.opener) {
 	console.log("window.opener is SET.");
+	window.opener.location = 'data:text/html;charset=UTF-8,<script>alert("window.opener vulnerability!");</script>';
 } else {
 	console.log("window.opener is NOT SET.");
 }
