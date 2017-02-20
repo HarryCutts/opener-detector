0e9208070cc0fa9ee6a5073be616a8a83c565899
Harry Cutts <eternal.linux@gmail.com>
2017-02-20 14:38:37 -0800

    Add a rudimentary ignore list

--- check.js
+++ check.js
@@ -1,5 +1,18 @@
 (function() {
 
+function checkIgnoreListFor(url) {
+	return browser.storage.local.get('ignoreList').then((result) => {
+		if (!result[0].ignoreList) return false;
+		// Work around Firefox <52 bug https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/storage/StorageArea/get#Return_value
+		const ignoreList = Array.isArray(result) ? result[0].ignoreList : result.ignoreList;
+		for (let ignoredURL of ignoreList) {
+			if (url.startsWith(ignoredURL)) return true;
+		}
+		return false;
+	});
+	// TODO: handle storage error
+}
+
 function getReportPageURL(sourceURL) {
 	const fragmentObj = {
 		sourceURL: sourceURL,
@@ -10,8 +23,14 @@ function getReportPageURL(sourceURL) {
 
 if (window.opener) {
 	const currentOpenerLocation = window.opener.location.toString();
-	console.log("window.opener is SET. The opener is currently at " + currentOpenerLocation);
-	window.opener.location = getReportPageURL(currentOpenerLocation);
+	checkIgnoreListFor(currentOpenerLocation).then((isOnIgnoreList) => {
+		if (!isOnIgnoreList) {
+			console.log("window.opener is SET. The opener is currently at " + currentOpenerLocation);
+			window.opener.location = getReportPageURL(currentOpenerLocation);
+		} else {
+			console.log("window.opener is SET, but the vulnerable page is on the ignore list.");
+		}
+	});
 } else {
 	console.log("window.opener is NOT SET.");
 }
--- vuln_report_page.js
+++ vuln_report_page.js
@@ -5,11 +5,40 @@ const fragmentObj = JSON.parse(decodeURIComponent(location.hash.slice(1)));
 const page = {
 	report: document.getElementById('report'),
 	sourcePageLink: document.getElementById('sourcePageLink'),
+	ignoreButton: document.getElementById('ignoreButton'),
 };
 
 page.sourcePageLink.setAttribute('href', fragmentObj.sourceURL);
 page.sourcePageLink.innerText = fragmentObj.sourceURL;
 
+page.ignoreButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	const urlWithoutFragment = removeFragment(fragmentObj.sourceURL);
+	addToIgnoreList(urlWithoutFragment).then(
+			() => {
+				alert("Vulnerabilities on the page " + urlWithoutFragment + " will no longer be reported.");
+			},
+	(error) => {
+		alert("There was a problem when adding " + urlWithoutFragment + " to the ignore list.");
+		console.error(error);
+	});
+});
+
 // Show the report element now that it's been filled out.
 page.report.classList.add('ready');
+
+function addToIgnoreList(url) {
+	return browser.storage.local.get('ignoreList').then((result) => {
+		const ignoreList = result.ignoreList || [];
+		if (ignoreList.indexOf(url) !== -1) return;
+		ignoreList.push(url);
+		return browser.storage.local.set({ignoreList});
+	});
+}
+
+function removeFragment(urlString) {
+	const url = new URL(urlString);
+	url.hash = '';
+	return url.toString();
+}
 })();
