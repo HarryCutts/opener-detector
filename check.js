7cc76016648b9eb469653b25e7a058b5c6fbe756
Harry Cutts <eternal.linux@gmail.com>
2017-02-20 11:50:49 -0800

    Open a bundled Web page on vulnerable tabs

--- check.js
+++ check.js
@@ -1,7 +1,7 @@
 (function() {
 if (window.opener) {
 	console.log("window.opener is SET.");
-	window.opener.location = 'data:text/html;charset=UTF-8,<script>alert("window.opener vulnerability!");</script>';
+	window.opener.location = browser.extension.getURL('vuln_report_page.html');
 } else {
 	console.log("window.opener is NOT SET.");
 }
